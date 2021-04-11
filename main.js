911d19aa962e0ef1b142c35e31fd2408cc13acba
なおゆき <nao@USERnoMacBook-puro.local>
2021-04-12 05:54:00 +0900

    developへのコミット4回目

--- main.js
+++ main.js
@@ -13,48 +13,48 @@ document.getElementById('insertBtn').onclick = function () {
 	}
 
 	//配列todosにオブジェクトtodoを格納
-	todos.push(todo)
+	todos.push(todo);
 	//タスク一覧を表示	
-	displayTodos()
+	displayTodos();
 
 }
 
 function displayTodos () {
 
-	const table = document.getElementById("table0")
-	table.innerHTML = ""
+	const table = document.getElementById("table0");
+	table.innerHTML = "";
 
 	for (let i = 0 ; i < todos.length ; i++){
 
 		//tr要素の追加
-		const tr = document.createElement('tr')
-		tr.id = `tr${i}`
-		table.appendChild(tr)
+		const tr = document.createElement('tr');
+		tr.id = `tr${i}`;
+		table.appendChild(tr);
 
 		//IDの追加
-		const td0 = document.createElement('td')
-		td0.textContent = `${i}`
-		tr.appendChild(td0)
+		const td0 = document.createElement('td');
+		td0.textContent = `${i}`;
+		tr.appendChild(td0);
 
 		//コメントの追加
-		const td1 = document.createElement('td')
-		td1.textContent = todos[i].task
-		tr.appendChild(td1)
+		const td1 = document.createElement('td');
+		td1.textContent = todos[i].task;
+		tr.appendChild(td1);
 
 		//状態ボタンの追加
-		const td2 = document.createElement('td')
-		const stateBtn = document.createElement('input')
-		stateBtn.type = 'button'
-		stateBtn.value = `${todos[i].status}`
-		td2.appendChild(stateBtn)
-		tr.appendChild(td2)
+		const td2 = document.createElement('td');
+		const stateBtn = document.createElement('input');
+		stateBtn.type = 'button';
+		stateBtn.value = `${todos[i].status}`;
+		td2.appendChild(stateBtn);
+		tr.appendChild(td2);
 
 		//削除ボタンの追加
-		const td3 = document.createElement('td')
-		const deleteBtn = document.createElement('input')
-		deleteBtn.type = 'button'
-		deleteBtn.value = '削除'
-		td3.appendChild(deleteBtn)
-		tr.appendChild(td3)
+		const td3 = document.createElement('td');
+		const deleteBtn = document.createElement('input');
+		deleteBtn.type = 'button';
+		deleteBtn.value = '削除';
+		td3.appendChild(deleteBtn);
+		tr.appendChild(td3);
 	}
 }
